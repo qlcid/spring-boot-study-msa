c3f575dd24cf0331e1fccdb6949642be210889d0
donggov <donggov@gmail.com>
2020-08-10 23:59:11 +0900

    스웨거 설명 추가

--- product/src/main/java/com/study/yeseul/product/controller/ProductController.java
+++ product/src/main/java/com/study/yeseul/product/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.study.yeseul.product.controller;
 import com.study.yeseul.product.domain.Product;
 import com.study.yeseul.product.service.ProductService;
 import com.study.yeseul.product.vo.ProductDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,18 +12,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+@Api(tags = "상품 APIs")
 public class ProductController {
 
     // spring 생성자 주입하는 방법이
     @Autowired
     private ProductService productService;
 
+    @ApiOperation(value = "상품목록 조회", notes = "상품목록 조회 API입니다.")
     @GetMapping("")
     public List<ProductDto.ProductDetailDto> getProductList() {
         return productService.getProductList();
     }
 
     // 등록(생성)
+    @ApiOperation(value = "상품등록", notes = "상품등록 API입니다. name에 iPhone을 입력하면 반값되는건 안비밀 ^^")
     @PostMapping("")
     public Product createProduct(@RequestBody final ProductDto.ProductCreateDto createDto) {
         return productService.createProduct(createDto);
